20f1cef9b2764ac3cc87e5af59592eb31cca74b8
GreemDev <greemdev@ryujinx.app>
2025-05-24 17:00:30 -0500

    fix: Super Mario Party Jamboree audio renderer crashing

--- src/Ryujinx.Audio/Renderer/Dsp/AdpcmHelper.cs
+++ src/Ryujinx.Audio/Renderer/Dsp/AdpcmHelper.cs
@@ -81,14 +81,14 @@ namespace Ryujinx.Audio.Renderer.Dsp
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static short GetCoefficientAtIndex(ReadOnlySpan<short> coefficients, int index)
         {
-            if ((uint)index >= (uint)coefficients.Length)
+            if ((uint)index < (uint)coefficients.Length)
             {
-                Logger.Error?.Print(LogClass.AudioRenderer, $"Out of bound read for coefficient at index {index}");
-
-                return 0;
+                return coefficients[index];
             }
 
-            return coefficients[index];
+            Logger.Error?.Print(LogClass.AudioRenderer, $"Out of bound read for coefficient at index {index}");
+
+            return 0;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
