2f400491d5fe0b7cd4521f22686e22f97f6d2ceb
KeatonTheBot <onikeaton@gmail.com>
2025-05-25 21:10:20 -0500

    ffmpeg: Fix green screen issues on Linux

--- src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/FFmpegApi.cs
+++ src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/FFmpegApi.cs
@@ -12,8 +12,8 @@ namespace Ryujinx.Graphics.Nvdec.FFmpeg.Native
 
         private static readonly Dictionary<string, (int, int)> _librariesWhitelist = new()
         {
-            { AvCodecLibraryName, (58, 61) },
-            { AvUtilLibraryName, (56, 59) },
+            { AvCodecLibraryName, (59, 60) },
+            { AvUtilLibraryName, (57, 58) },
         };
 
         private static string FormatLibraryNameForCurrentOs(string libraryName, int version)
